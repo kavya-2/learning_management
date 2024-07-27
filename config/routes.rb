0cb1a2dd55d9040cd17a6afb756182b4db34b79e
kavya-2 <kavya.sivapal@gmail.com>
2024-07-27 15:37:29 +0530

    feat: Add Devise gem for User and Admin

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+  resources :courses
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "courses#index"
+end
